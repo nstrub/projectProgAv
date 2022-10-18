11b9a71ae33374a15006b3abd1b500cb131b44c2
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-10-18 16:15:43 +0200

    Structuration du projet + affichage d'un sprite deplacement en cours...

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "define.hpp"
-
+#include "game.hpp"
 
 int main() {
     sf::RenderWindow app(sf::VideoMode(WINDOW_X, WINDOW_Y), "Tesoro");
     app.setFramerateLimit(60);
 
+    Game game = Game();
+
     while (app.isOpen()) {
-        
+        game.handleMoves(app);
+        game.draw(app);
     }
 
     return 0;
